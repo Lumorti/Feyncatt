4e619dfeeda60c681b63e992a54c66881410d5fb
Lumorti <lbmortimer1998@gmail.com>
2020-03-11 08:04:50 +0000

    added basic test firmware

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,24 +1,60 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+uint8_t servoNum = 0;
+
+void setup(){
+
+  // Setup serial
+  Serial.begin(9600);
+  Serial.println("started");
+
+  // Setup servo control
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);  // The int.osc. is closer to 27MHz  
+  pwm.setPWMFreq(SERVO_FREQ);
+
+  // Setup SD card reading TODO 1
+
+  delay(10);
+  
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+void setServoAngle(uint8_t n, double angle){
+  
+  
+}
+
+void changeToState(){
+  
+  
+}
+
+void loop(){
+  
+  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++){
+    
+    pwm.setPWM(servoNum, 0, pulselen);
+    
+  }
+
+  delay(500);
+  
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--){
+    
+    pwm.setPWM(servoNum, 0, pulselen);
+    
+  }
+
+  delay(500);
+
+  servoNum++;
+  if (servoNum > 7) servoNum = 0;
+  
 }
